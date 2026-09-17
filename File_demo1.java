b9811f1b41e83623179f67dd91b7be9a7b282967
qiduban <3400520665@qq.com>
2026-09-17 23:47:12 +0800

    Add files via upload

--- File_demo1.java
+++ File_demo1.java
@@ -2,6 +2,7 @@ package com.java45;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class File_demo1 {
     //File 对象本身并不代表文件里的内容，它只是描述一个文件/目录的路径
@@ -12,8 +13,33 @@ public class File_demo1 {
     C:\Users\alienware\Desktop
     */
     public static void main(String[] args) throws IOException {
-        File file = new File("114514.txt");
-        file.createNewFile();//真正创建文件而不是简单的创建一个人FILE类型对象
+        File file = new File("C:\\Users\\34005\\Desktop\\code\\114514.txt");
+        File file2 =new File("C:\\Users\\34005\\Desktop\\code\\aaa");
+        file.createNewFile();
+        boolean a = file2.mkdir();
+        System.out.println(a);
+        File file3 =new File("C:\\Users\\34005\\Desktop\\code\\aaa\\bb\\cc\\ee");
+        boolean b = file3.mkdirs();
+        System.out.println(b);
+        boolean c =file.delete();
+        System.out.println(c);
+        File file4 = new File("C:\\Users\\34005\\Desktop\\code\\aaa\\bb\\cc\\ee");
+        boolean e = file4.delete();
+        System.out.println(e);
+        File file5 = new File("C:\\Users\\34005\\Desktop\\code\\aaa\\bb");
+        boolean f = file5.delete();
+        System.out.println(f);
+        //真正创建文件而不是简单的创建一个人FILE类型对象
+        //如果文件路径下的文件不存在，则创建成功，返回true
+        //----------------存在，则创建失败，返回false
+        //-----------的路径错误，则报错返回IOexception
+        //createNewFile方法创建的一定是文件，若当前文件路径不包含和后缀则会创建一个没有后缀的文件
+        //.mkdir()     make directory创建目录的方法
+        //.mkdir       windows的文件路径是唯一的，若当前路径已经存在，则返回创建失败
+        //.mkdir      只能创建单级文件夹
+        //.mkdirs     可用于创建单级文件夹和多级文件夹
+        //.delete     用于删除File类型的对象（不走回收站)
+        //.delete     删除文件夹时删除有内容的文件夹会失败，只能删除无内容的文件夹
 //————————————————————————————————————— 获取文件名称 ——————————————————————————————————————————
         System.out.println(file.getName());
 //————————————————————————————————————— 获取文件路径 ——————————————————————————————————————————
@@ -45,55 +71,21 @@ public class File_demo1 {
         System.out.println(file1.isFile());
         System.out.println(size);//返回文件大小（字节大小）
         System.out.println(file1.lastModified());//返回文件最后的修改时间1789369534103
-        time(file1.lastModified());
+        // ——————————————————————————————————————— 文件获取并遍历 ————————————————————————————————————————————
+        //public File[] listfiles()         `    获取路径下的所有内容(包括隐藏内容)
+        File file7 =new File("C:\\Users\\34005\\Desktop\\code");
+        File []files = file7.listFiles();
+        Arrays.stream(files).forEach(System.out::println);
+        File []files1 = file7.listRoots();//获取系统盘符
+        Arrays.stream(files1).forEach(System.out::println);
+        /*
+        • 当调用者File表示的路径不存在时，返回null
+        • 当调用者File表示的路径是文件时，返回null
+        • 当调用者File表示的路径是一个空文件夹时，返回一个长度为0的数组
+        • 当调用者File表示的路径是一个有内容的文件夹时，将里面所有文件和文件夹的路径放在File数组中返回
+        • 当调用者File表示的路径是一个有隐藏文件的文件夹时，将里面所有文件和文件夹的路径放在File数组中返回，包含隐藏文件
+        ● 当调用者File表示的路径是需要权限才能访问的文件夹时，返回null
+         */
+    }
 
-    }
-    public boolean fun2(long year){
-        if(year / 4 == 0 || year /400 == 0){
-            return true;
-        }
-      return false;
-    };
-    public long fun(long day){
-        long year = 1970;
-        long month = 1;
-        long Day = 1;
-        if(fun2(year)){
-            Day = day / 366;
-            year += Day;
-            while(Day > 30){
-                int i = 1;
-                if(i > 12){
-                    i =1;
-                }switch (i){
-                    case 1:Day = Day - 31;
-                    case 2:Day = Day - 28;
-                    case 3:Day = Day - 31;
-                    case 4:Day = Day - 30;
-                    case 5:Day = Day - 31;
-                    case 6:Day = Day - 30;
-                    case 7:Day = Day - 31;
-                    case 8:Day = Day - 31;
-                    case 9:Day = Day - 30;
-                    case 10:Day = Day - 31;
-                    case 11:Day = Day - 30;
-                    case 12:Day = Day - 31;
-                }
-                i++;
-            }
-        }else{
-            Day = day / 365;
-            year += Day;
-        }
-        return Day;
-    }
-    public static void time(long Time){
-        long ms = Time % 1000;
-        long day = Time / 1000 / 60 / 60 / 24;
-        long sec = (Time / 1000) % 86400;
-        long hour = sec / 3600;
-        long min = sec % 3600 / 60;
-        long Sec = sec % 3600 % 60;
-        System.out.println(hour + " " + min + " " + Sec);
-    }
 }
